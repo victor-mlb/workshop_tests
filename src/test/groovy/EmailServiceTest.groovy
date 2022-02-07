21e009acb038290150500be9b3945863c4cea0e3
Renan Neri <renan.neri@mercadolivre.com>
2022-02-07 16:55:59 -0300

    solved email test.

--- src/test/groovy/EmailServiceTest.groovy
+++ src/test/groovy/EmailServiceTest.groovy
@@ -2,8 +2,11 @@ import api.EmailApi
 import model.Email
 import org.mockito.Mockito
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class EmailServiceTest extends Specification {
+
+    private String
 
     def "should return ordered list"() {
         given:
@@ -27,5 +30,64 @@ class EmailServiceTest extends Specification {
                     new Email(3L, 'cbc@mail.com'),
                     new Email(1L, 'dbc@mail.com')
             ]
+    }
+
+    def "should save email"(){
+        given:
+            String email = "test@email.com"
+        when:
+            EmailApi mockEmailApi = Mockito.mock(EmailApi)
+            EmailService emailService = new EmailService(mockEmailApi)
+            def emailResponse = emailService.save(email)
+        then:
+            emailResponse != null
+
+    }
+
+    @Unroll
+    def "should exception save email"(){
+        when:
+            EmailApi mockEmailApi = Mockito.mock(EmailApi)
+            EmailService emailService = new EmailService(mockEmailApi)
+            def emailResponse = emailService.save(email)
+        then:
+             def ex = thrown(RuntimeException)
+             ex.message == expectedMessage
+        where:
+            email | expectedMessage
+            null  | "Email should not be empty"
+            ''    | "Email should not be empty"
+    }
+
+    def "should update email"(){
+        given:
+             EmailApi mockEmailApi = Mockito.mock(EmailApi)
+             Mockito.when(mockEmailApi.get(Mockito.anyLong())).thenReturn(new Email(10l, "test@email.com"))
+             EmailService emailService = new EmailService(mockEmailApi)
+        when:
+             def result = emailService.update(10l,"test@email.com")
+        then:
+            result.id == 10l
+            result.email == "test@email.com"
+
+    }
+
+    @Unroll
+    def "should exception update email"(){
+        when:
+            EmailApi mockEmailApi = Mockito.mock(EmailApi)
+            EmailService emailService = new EmailService(mockEmailApi)
+            def result = emailService.update(id, email)
+        then:
+            def ex = thrown(RuntimeException)
+            ex.message == expectedResult
+        where:
+            id     | email              | expectedResult
+            1      | null               | "Email should not be empty"
+            1      | ''                 | "Email should not be empty"
+            null   | "test@email.com"  | "ID should not be empty"
+            -1     | "test@email.com"  | "ID should not be empty"
+
+
     }
 }
