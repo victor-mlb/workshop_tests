fbe8587086b51cb9f73f0a534f02a2950324bce7
Kelvin Carvalho <kelvin.carvalho@mercadolivre.com>
2022-03-14 10:19:49 -0300

    test: add calculator

--- src/test/groovy/CalculatorTest.groovy
+++ src/test/groovy/CalculatorTest.groovy
@@ -24,4 +24,19 @@ class CalculatorTest extends Specification {
             1.0f    | 1.0f    | 1f
             1.0f    | 0f      | 0f
     }
+
+    @Unroll
+    def "should absolute sum"() {
+        when:
+        Integer result = Calculator.absoluteSum(valueA, valueB)
+        then:
+        result == expectedResult
+        where:
+        valueA | valueB | expectedResult
+        1    | 1    | 2
+        -1   | -5   | 6
+        null | 1    | null
+        1    | null | null
+        null | null | null
+    }
 }
