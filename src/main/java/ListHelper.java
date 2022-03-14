e0d3b5be8eb8eef07a9b1ebad67fbeb27faf4717
Kelvin Carvalho <kelvin.carvalho@mercadolivre.com>
2022-03-14 10:21:27 -0300

    feat: add list helper

--- src/main/java/ListHelper.java
+++ src/main/java/ListHelper.java
@@ -1,3 +1,10 @@
+import api.ListApi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 /**
  *
  * Your task is to process a sequence of integer numbers to determine the following statistics:
@@ -20,5 +27,49 @@
  * Get the list of numbers from an API (check EmailApi for examples)
  */
 public class ListHelper {
+
+    public static final Integer SIZE_VALIDATE = 6;
+    public static final Integer AVERAGE_VALIDATE = 20;
+    public static final String MESSAGE_ERROR_LIST = "Empty or null list";
+
+    private ListApi list;
+
+    public ListHelper(ListApi list) {
+        this.list = list;
+    }
+
+    public List<Integer> getList() {
+        List<Integer> listApi = list.fetchList();
+
+        if(listApi == null || listApi.isEmpty())
+            throw new NoSuchElementException(MESSAGE_ERROR_LIST);
+
+        return listApi;
+    }
+
+    public Integer getMinValue() {
+       return getList().stream().mapToInt(v -> v).min().orElseThrow(NoSuchElementException::new);
+    }
+
+    public Integer getMaxValue() {
+        return getList().stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new);
+    }
+
+    public Integer getSize() {
+        return getList().size();
+    }
+
+    public Double getAverage() {
+        return getList().stream().mapToInt(v -> v).average().orElseThrow(NoSuchElementException::new);
+    }
+
+    public Boolean isValid() {
+        if (getSize() == SIZE_VALIDATE && getAverage() >= AVERAGE_VALIDATE)
+            return true;
+
+        return false;
+    }
+
+
 
 }
---
+++ src/main/java/api/ListApi.java
@@ -0,0 +1,13 @@
+package api;
+
+import model.Email;
+
+import java.util.List;
+
+public class ListApi {
+
+    public List<Integer> fetchList() {
+        throw new RuntimeException("DON'T IMPLEMENT NOR REMOVE IT");
+    }
+
+}
