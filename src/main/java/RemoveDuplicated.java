8d0c78650cfa3e3f6cdf0e7b8a70f245c8528654
Kelvin Carvalho <kelvin.carvalho@mercadolivre.com>
2022-03-14 10:22:22 -0300

    feat: remove duplicated list

--- src/main/java/RemoveDuplicated.java
+++ src/main/java/RemoveDuplicated.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Define a function RemoveDuplicate(nlist) to remove duplicate elements from list.
  *  Here are some examples:
@@ -7,5 +11,12 @@
  *      [1,1,2,2,3,3] -> [1,2,3]
  **/
 public class RemoveDuplicated {
+
+    public static List<Integer> removeDuplicate(List<Integer> list) {
+        if(list == null || list.isEmpty())
+            return Collections.emptyList();
+
+        return list.stream().distinct().collect(Collectors.toList());
+    }
 
 }
